7a360b5bfc787dc7b30edea42a78b95ead245367
Diego S <dsanchezme@unal.edu.co>
2021-05-16 11:31:31 -0500

    Quick fix in Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,107 +1,183 @@
 <template>
-    
-    <div class="login">
-        <div class="container mb-5 pt-4 text-center">
-            <div class="pb-5">
-                <img class="app-logo" src="../assets/oriun.png" alt="" height="100" />
-            </div>
-            <div class="card mx-auto">
-                <div class="card-header border-bottom-0 bg-transparent">
-                    <ul class="nav nav-tabs justify-content-center" id="pills-tab" role="tablist">
-                        <li class="nav-item text-center" id="login-tab">
-                            <a class="nav-link active text-primary" id="pills-login-tab" data-toggle="pill"
-                                href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Log
-                                In</a>
-                        </li>
+  <div class="login">
+    <div class="container mb-5 pt-4 text-center">
+      <div class="pb-5">
+        <img class="app-logo" src="../assets/oriun.png" alt="" height="100" />
+      </div>
+      <div class="card mx-auto">
+        <div class="card-header border-bottom-0 bg-transparent">
+          <ul
+            class="nav nav-tabs justify-content-center"
+            id="pills-tab"
+            role="tablist"
+          >
+            <li class="nav-item text-center" id="login-tab">
+              <a
+                class="nav-link active text-primary"
+                id="pills-login-tab"
+                data-toggle="pill"
+                href="#pills-login"
+                role="tab"
+                aria-controls="pills-login"
+                aria-selected="true"
+                >Log In</a
+              >
+            </li>
 
-                        <li class="nav-item text-center">
-                            <a class="nav-link text-primary" id="pills-register-tab" data-toggle="pill"
-                                href="#pills-register" role="tab" aria-controls="pills-register"
-                                aria-selected="false">Sign Up</a>
-                        </li>
-                    </ul>
+            <li class="nav-item text-center">
+              <a
+                class="nav-link text-primary"
+                id="pills-register-tab"
+                data-toggle="pill"
+                href="#pills-register"
+                role="tab"
+                aria-controls="pills-register"
+                aria-selected="false"
+                >Sign Up</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div class="card-body pb-4">
+          <div class="tab-content" id="pills-tabContent">
+            <div
+              class="tab-pane fade show active"
+              id="pills-login"
+              role="tabpanel"
+              aria-labelledby="pills-login-tab"
+            >
+              <div class="placeholder">Username</div>
+              <form v-on:submit.prevent="submitFormLogin">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="user_name"
+                    class="form-control"
+                    id="username"
+                    v-model="loginform.user_name"
+                    required
+                    autofocus
+                  />
+                </div>
+                <div class="placeholder">Password</div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    name="password"
+                    class="form-control"
+                    id="password"
+                    v-model="loginform.password"
+                    required
+                  />
+                </div>
+                <div class="text-center pt-4">
+                  <vue-hcaptcha
+                    sitekey="e3605ee2-18a4-4e7c-9a8e-5885075be08e"
+                    @verify="captchaVerified"
+                    @error="hCaptchaVerified = false"
+                  >
+                  </vue-hcaptcha>
+                </div>
+                <div class="text-center pt-4">
+                  <button type="submit" class="btn btn-primary">Login</button>
                 </div>
 
-                <div class="card-body pb-4">
-                    <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-login" role="tabpanel"
-                            aria-labelledby="pills-login-tab">
-                            <div class="placeholder">Username</div>
-                            <form  v-on:submit.prevent="submitFormLogin">
-                                <div class="form-group">
-                                    <input type="text" name="user_name" class="form-control" id="username" 
-                                    v-model="loginform.user_name" required autofocus />
-                                </div>
-                                <div class="placeholder">Password</div>
-                                <div class="form-group">
-                                    <input type="password" name="password" class="form-control" id="password"
-                                        v-model="loginform.password" required />
-                                </div>
-                                <div class="text-center pt-4">
-                                <vue-hcaptcha
-                                    sitekey="e3605ee2-18a4-4e7c-9a8e-5885075be08e"
-                                    @verify="captchaVerified"
-                                    @error="hCaptchaVerified=false">
-                                </vue-hcaptcha>        
-                                </div>
-                                <div class="text-center pt-4">
-                                    <button type="submit" class="btn btn-primary">Login</button>
-                                </div>
-
-                                <div class="text-center pt-3">
-                                    <!-- <a class="btn btn-link text-primary" href="#"
+                <div class="text-center pt-3">
+                  <!-- <a class="btn btn-link text-primary" href="#"
                         >Forgot Your Password?</a
                       > -->
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="tab-pane fade" id="pills-register" role="tabpanel"
-                            aria-labelledby="pills-register-tab">
-                            <form v-on:submit.prevent="submitFormRegister">
-                                <div class="form-group">
-                                    <input type="text" name="username" id="name" class="form-control"
-                                        placeholder="Username" v-model="registerform.user_name" required autofocus />
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="email" name="email" id="email" class="form-control" placeholder="Email"
-                                    v-model="registerform.email" required />
-                                </div>
-
-                                <div class="form-group">
-                                    <input type="password" name="password" id="password" class="form-control"
-                                        v-model="registerform.password" placeholder="Set a password" required />
-                                </div>
-                                <div class="text-center pt-2 pb-1 container-hint">
-                                	<transition name="hint" appear>
-                                    <div v-if='passwordValidation.errors.length > 0' class='hints'>
-                                      <h5>Hints</h5>
-                                      <h5 class="reg-validation" v-for='error in passwordValidation.errors' :key="error.id">{{error}}<br></h5>
-                                    </div>
-                                    <div v-else >
-                                      <h5>El password es valido</h5>
-                                    </div>
-                                  </transition>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" name="password_confirmation" id="password-confirm"
-                                        class="form-control" placeholder="Confirm password" 
-                                        v-model="registerform.password_confirmation" required />
-                                </div>
-
-                                <div class="text-center pt-2 pb-1">
-                                    <button type="submit" class="btn btn-primary">
-                                        Register
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
                 </div>
+              </form>
             </div>
+
+            <div
+              class="tab-pane fade"
+              id="pills-register"
+              role="tabpanel"
+              aria-labelledby="pills-register-tab"
+            >
+              <form v-on:submit.prevent="submitFormRegister">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="username"
+                    id="name"
+                    class="form-control"
+                    placeholder="Username"
+                    v-model="registerform.user_name"
+                    required
+                    autofocus
+                  />
+                </div>
+
+                <div class="form-group">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    class="form-control"
+                    placeholder="Email"
+                    v-model="registerform.email"
+                    required
+                  />
+                </div>
+
+                <div class="form-group">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    class="form-control"
+                    v-model="registerform.password"
+                    placeholder="Set a password"
+                    required
+                  />
+                </div>
+                <div class="text-center pt-2 pb-1 container-hint">
+                  <transition name="hint" appear>
+                    <div
+                      v-if="passwordValidation.errors.length > 0"
+                      class="hints"
+                    >
+                      <h5>Hints</h5>
+                      <h5
+                        class="reg-validation"
+                        v-for="error in passwordValidation.errors"
+                        :key="error.id"
+                      >
+                        {{ error }}<br />
+                      </h5>
+                    </div>
+                    <div v-else>
+                      <h5>El password es valido</h5>
+                    </div>
+                  </transition>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    name="password_confirmation"
+                    id="password-confirm"
+                    class="form-control"
+                    placeholder="Confirm password"
+                    v-model="registerform.password_confirmation"
+                    required
+                  />
+                </div>
+
+                <div class="text-center pt-2 pb-1">
+                  <button type="submit" class="btn btn-primary">
+                    Register
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template> 
 
 <script>
@@ -129,7 +205,8 @@ export default {
                 password:""
             }, 
             validregex: false,
-            hCaptchaVerified: false
+            hCaptchaVerified: false,
+            accountVerified = false
         }
     }, 
     computed:{
@@ -154,22 +231,32 @@ export default {
       submitFormLogin: function(){
         //alert(JSON.stringify(this.loginform))
         if (this.hCaptchaVerified == true){
-        var self = this
-         axios
-          .post("https://wise-brook-308119.ue.r.appspot.com/userlog/",self.loginform)
-          .then((result)=>{
-            self.$emit('logeado', self.loginform.user_name, result.data.ROL_NAME)
-          })
-          .catch((error) => {
-              if (error.response.status == "404")
-                  alert("ERROR 404:Contraseña Erronea.");
-              if (error.response.status == "406")
-                  alert("ERROR 403: Usuario no encontrado."); 
-              this.hCaptchaVerified=false; 
-         });
+          var self = this
+          await axios
+            .get("http://localhost:8081/userstate?user=" + self.loginform.user_name)
+            .then((result) => {
+              self.accountVerified = result;
+            })
+
+          if(self.accountVerified){
+          axios
+            .post("https://wise-brook-308119.ue.r.appspot.com/userlog/",self.loginform)
+            .then((result)=>{
+              self.$emit('logeado', self.loginform.user_name, result.data.ROL_NAME)
+            })
+            .catch((error) => {
+                if (error.response.status == "404")
+                    alert("ERROR 404:Contraseña Erronea.");
+                if (error.response.status == "406")
+                    alert("ERROR 403: Usuario no encontrado."); 
+                this.hCaptchaVerified=false; 
+          });
+          }else{
+            alert("No has verificado tu cuenta.")            
+          }
         }
         else{
-          alert("El Captcha no se ha verificado o se hiso de manera incorrecta")
+          alert("El Captcha no se ha verificado o se hizo de manera incorrecta")
         }
          
       }, 
@@ -207,124 +294,120 @@ export default {
 
 
 <style scoped>
-
-*{
-    font-size: 14pt;
+* {
+  font-size: 14pt;
 }
 
-
 .card {
-    width: 350px;
-    background-color: #94B43B;
-    border-radius: 30px;
-    
-  }
-  
-  button, .btn-primary{
-    background-color: #B1B2B0;
-    border: none;
-    border-radius: 10px;
-    margin-top: 40px;
-    width: 80%;
-    margin-bottom: 20px;
-  }
-  
-  .form-control {
-    border-radius: 10px;
-    width: 80%;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  
-  .placeholder {
-    float: left;
-    /* width:100px; */
-    color: white;
-    margin-top: 5px;
-    margin-bottom: 15px;
-    margin-left: 30px;
-  }
-  
-  .card-header {
-    padding: 0;
-  }
-  
-  li.nav-item  {
-    /* padding: auto; */
-    width: 50%;
-    /* border-top-left-radius: 15px; */
-    margin-top: -1px;
-  }
-  
-  .nav-tabs .nav-item.show,
-  .nav-tabs .nav-link.active {
-    background-color: transparent;
-    border: none;
-    color: white;
-  }
-  
-  .nav-link {
-    background-color: #466B3F;
-    border-bottom: 1px;
-  }
-  
-  #pills-login-tab {
-    border-top-left-radius: 30px;
-    /* margin: -1px -1px 0px 0px; */
-    margin-left: -1px;
-  }
-  
-  #pills-register-tab {
-    border-top-right-radius: 30px;
-    margin-right: -1px;
-    /* border-bottom: 1px solid white; */
-    /* border-left: 0.1px solid white; */
-  }
-  
-  .nav-tabs .nav-link:focus,
-  .nav-tabs .nav-link:hover {
-    border-color: transparent;
-  }
-  
-  .nav-tabs .nav-link {
-    border-top-left-radius: 0px;
+  width: 350px;
+  background-color: #94b43b;
+  border-radius: 30px;
+}
+
+button,
+.btn-primary {
+  background-color: #b1b2b0;
+  border: none;
+  border-radius: 10px;
+  margin-top: 40px;
+  width: 80%;
+  margin-bottom: 20px;
+}
+
+.form-control {
+  border-radius: 10px;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.placeholder {
+  float: left;
+  /* width:100px; */
+  color: white;
+  margin-top: 5px;
+  margin-bottom: 15px;
+  margin-left: 30px;
+}
+
+.card-header {
+  padding: 0;
+}
+
+li.nav-item {
+  /* padding: auto; */
+  width: 50%;
+  /* border-top-left-radius: 15px; */
+  margin-top: -1px;
+}
+
+.nav-tabs .nav-item.show,
+.nav-tabs .nav-link.active {
+  background-color: transparent;
+  border: none;
+  color: white;
+}
+
+.nav-link {
+  background-color: #466b3f;
+  border-bottom: 1px;
+}
+
+#pills-login-tab {
+  border-top-left-radius: 30px;
+  /* margin: -1px -1px 0px 0px; */
+  margin-left: -1px;
+}
+
+#pills-register-tab {
+  border-top-right-radius: 30px;
+  margin-right: -1px;
+  /* border-bottom: 1px solid white; */
+  /* border-left: 0.1px solid white; */
+}
+
+.nav-tabs .nav-link:focus,
+.nav-tabs .nav-link:hover {
+  border-color: transparent;
+}
+
+.nav-tabs .nav-link {
+  border-top-left-radius: 0px;
+}
+
+.nav-tabs {
+  border-bottom: none;
+}
+
+.container-hint h5 {
+  color: white;
+  font-size: small;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.text-primary {
+  color: #7a7a7a !important;
+}
+
+.text-primary.active {
+  color: white !important;
+}
+
+@media (max-width: 470px) {
+  .app-logo {
+    max-width: 400px;
+    width: 70%;
+    height: auto;
   }
 
-  .nav-tabs{
-    border-bottom: none;
+  .card {
+    max-width: 400px;
+    width: 80%;
   }
+}
 
-  .container-hint h5{
-    color: white;
-    font-size: small;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  
- 
-  .text-primary {
-    color: #7a7a7a !important;
-  }
-  
-  .text-primary.active {
-    color: white !important;
-  }
-  
-  @media (max-width: 470px) {
-    .app-logo{
-        max-width: 400px;
-        width: 70%;
-        height: auto;
-    }
-  
-    .card{
-        max-width: 400px;
-        width: 80%;
-        
-    }
-  }
-  
-  /* @media only screen and (max-width: 600px) {
+/* @media only screen and (max-width: 600px) {
     .app-logo{
         max-width: 400px;
         width: 70%;
