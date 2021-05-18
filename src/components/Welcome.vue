cadeee072964d043b61b144c54228f6aa8594019
Diego Sanchez <48555473+dsanchezme@users.noreply.github.com>
2021-05-17 20:58:16 -0500

    Deleting event from user

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -8,7 +8,7 @@
       </h6>
     </div>
     <div class="container">
-      <div class="row">
+      <div class="row w-100">
         <div class="col-md-6 mb-6">
           <div class="mis-eventos">
             <h4 class="titulo-eventos">Mis eventos de esta semana</h4>
@@ -19,17 +19,26 @@
                 :key="event.id"
                 class="tar-evento evento flex-fill bd-highlight"
               >
-                <h4 class="info-titulo">
-                  <a
-                    href="#"
-                    class="info-titulo-ref detalles"
-                    v-on:click="
-                      pop = 'evento';
-                      mostrar(event);
-                    "
-                    >{{ event.event_TITLE }}</a
+                <div class="ml-3 row">
+                  <h4 class="info-titulo">
+                    <a
+                      href="#"
+                      class="info-titulo-ref detalles"
+                      v-on:click="
+                        pop = 'evento';
+                        mostrar(event);
+                      "
+                      >{{ event.event_TITLE }}</a
+                    >
+                  </h4>
+                  <button
+                    type="button"
+                    class="btn btn-danger quitar ml-auto mr-4"
+                    @click="delEvent(event)"
                   >
-                </h4>
+                    -
+                  </button>
+                </div>
                 <p class="info-fecha">fecha: {{ event.event_INIT }}</p>
                 <p class="info-hora-lugar">
                   hora: {{ event.event_INIT_HOUR }} Lugar:
@@ -109,16 +118,24 @@
         <h6>Locación: {{ locacionEvento }}</h6>
       </div>
     </div>
+    <!-- MODAL -->
+    <vue-toastr ref="mytoast"></vue-toastr>
+    <confirm-dialogue ref="confirmDialogue"></confirm-dialogue>
   </div>
 </template>
 
 <script>
 import NavBar from "./header/NavBar";
 import axios from "axios";
+import ConfirmDialogue from "../components/modal/ConfirmDialogue";
 // import { events } from "../fake-data";
 export default {
   name: "Welcome",
-  components: { NavBar },
+  components: {
+    NavBar,
+    ConfirmDialogue,
+    "vue-toastr": window.VueToastr,
+  },
   data: function () {
     return {
       username: "",
@@ -148,10 +165,7 @@ export default {
     getEvents() {
       // console.log(this.username, "hola", self.username);
       axios
-        .get(
-          "https://wise-brook-308119.ue.r.appspot.com/userevents/?user=" +
-            this.username
-        )
+        .get("https://wise-brook-308119.ue.r.appspot.com/userevents/?user=" + this.username)
         .then((response) => {
           // axios
           // .get("http://localhost:8081/userevents/?user=" + this.username)
@@ -164,10 +178,7 @@ export default {
     getUserSports() {
       // console.log(this.username, "hola", self.username);
       axios
-        .get(
-          "https://wise-brook-308119.ue.r.appspot.com/usersports/?user=" +
-            this.username
-        )
+        .get("https://wise-brook-308119.ue.r.appspot.com/usersports/?user=" + this.username)
         .then((response) => {
           // axios
           //    .get("http://localhost:8081/usersports/?user=" + this.username)
@@ -225,7 +236,7 @@ export default {
               "&sport=" +
               this.deporteNuevo
           )
-          // .get("https://wise-brook-308119.ue.r.appspot.com/sports")
+          // .get("http://localhost:8081/sports")
           .then((response) => {
             // axios
             //    .post(
@@ -234,7 +245,7 @@ export default {
             //          "&sport=" +
             //          this.deporteNuevo
             //    )
-            //    // .get("https://wise-brook-308119.ue.r.appspot.com/sports")
+            //    // .get("http://localhost:8081/sports")
             //    .then((response) => {
             console.log(response);
             // this.userSport = response.data;
@@ -266,7 +277,7 @@ export default {
           alert("deporte eliminado correctamente");
           location.href = "../";
         })
-        // .get("https://wise-brook-308119.ue.r.appspot.com/sports")
+        // .get("http://localhost:8081/sports")
         // .then((response) => {
         //    console.log(response);
         //    this.userSport = response.data;
@@ -278,6 +289,44 @@ export default {
       this.agregarDep = false;
 
       // console.log("hola" + deporte);
+    },
+    async delEvent(event) {
+      // this.printHello()
+      const ok = await this.$refs.confirmDialogue.show({
+        title: "Eliminar Evento",
+        message:
+          "¿Está seguro que desea eliminar el Evento? </br> Esta acción es irreversible.",
+        okButton: "Eliminar",
+      });
+      // If you throw an error, the method will terminate here unless you surround it wil try/catch
+      if (ok) {
+        try {
+          await axios.delete(
+            "https://wise-brook-308119.ue.r.appspot.com/NoEvent?id_event=" +
+              event.id_EVENT
+          );
+          document.getElementById(event.id_EVENT).remove();
+          this.success("Evento eliminado");
+          setTimeout(() => {
+            location.reload();
+          }, 1000);
+        } catch (error) {
+          this.error("Error eliminando evento");
+        }
+      } else {
+      }
+    },
+    success(msg) {
+      this.$refs.mytoast.s({
+        msg: msg,
+        progressbar: false,
+      });
+    },
+    error(msg) {
+      this.$refs.mytoast.e({
+        msg: msg,
+        progressbar: false,
+      });
     },
   },
 };
