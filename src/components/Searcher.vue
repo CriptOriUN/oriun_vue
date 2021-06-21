2e89de6525abe4e209bf466307470a1aaba0954d
nrrf <nrrojasfe@unal.edu.co>
2021-06-21 16:49:32 -0500

    fixed searcher

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -1,140 +1,200 @@
 <template>
-
-    <div class="createevent">
-        <NavBar :username="username"/>
-        <div class="container d-flex justify-content-center">
-            <div class="row my-3 ">
-                <h2>Buscador</h2>
-            </div>
-        </div>
-        <div class="container d-flex justify-content-center">
-            <div class="row my-3 ">
-            <input type="text" name="key-words" id="key-words"
-                class="form-control" placeholder="Palabras Clave" v-model="search" />
-            </div>
-        </div>
-        <div class="plantilla">
-            <div class="container contenedor_p">
-                <div class="eventos" >
-                    <div class="evento" v-for="evento in filterEvents" :key="evento.id"> 
-                        <div class = "columnas">
-                            <div>
-                                <h4>Nombre: {{evento.event_TITLE}}</h4>
-                                <h4>Deporte: {{evento.name_SPORT}}</h4>
-                                <h4>Organizador: {{evento.user_NAME}}</h4>
-                            </div> 
-                            <div class="center-column">
-                                <div>
-                                    <h4>Capacidad: {{evento.capacity}}</h4>
-                                    <h4>Fecha Inicio: {{evento.event_INIT}}</h4>
-                                    <h4>Fecha Fin: {{evento.event_END}}</h4>
-                                </div>
-                            </div>
-                            <div>
-                                <h4>Locación: {{evento.name_LOC_SPORT}}</h4>
-                                <h4>Hora Inicio: {{evento.event_INIT_HOUR}}</h4>
-                                <h4>Hora Fin: {{evento.event_FINISH_HOUR}}</h4>
-                            </div>
-                        </div>
-                        <div id="boton-asistir">
-                            <button type="button" class="btn btn-primary" @click="asistir(evento.id_EVENT)">Asistir</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="createevent">
+    <NavBar :username="username" />
+    <div class="container d-flex justify-content-center">
+      <div class="row my-3">
+        <h2>Buscador</h2>
+      </div>
     </div>
+    <div class="container d-flex justify-content-center">
+      <div class="row my-3">
+        <input
+          type="text"
+          name="key-words"
+          id="key-words"
+          class="form-control"
+          placeholder="Palabras Clave"
+          v-model="search"
+        />
+      </div>
+    </div>
+    <div v-if="isLoading">
+      <!--<p> Cargando... </p>-->
+      <div class="loading-icon">
+        <Spinner :color="color" />
+      </div>
+    </div>
+    <div v-else class="plantilla">
+      <div class="container contenedor_p">
+        <div class="eventos">
+          <div class="evento" v-for="evento in filterEvents" :key="evento.id">
+            <div class="columnas">
+              <div>
+                <h4>Nombre: {{ evento.event_TITLE }}</h4>
+                <h4>Deporte: {{ evento.name_SPORT }}</h4>
+                <h4>Organizador: {{ evento.user_NAME }}</h4>
+              </div>
+              <div class="center-column">
+                <div>
+                  <h4>Capacidad: {{ evento.capacity }}</h4>
+                  <h4>Fecha Inicio: {{ evento.event_INIT }}</h4>
+                  <h4>Fecha Fin: {{ evento.event_END }}</h4>
+                </div>
+              </div>
+              <div>
+                <h4>Locación: {{ evento.name_LOC_SPORT }}</h4>
+                <h4>Hora Inicio: {{ evento.event_INIT_HOUR }}</h4>
+                <h4>Hora Fin: {{ evento.event_FINISH_HOUR }}</h4>
+              </div>
+            </div>
+            <div id="boton-asistir">
+              <button
+                class="btn btn-primary"
+                type="button"
+                data-toggle="collapse"
+                :data-target="'#details' +  evento.id_EVENT"
+                aria-controls="details"
+                aria-expanded="false"
+                title="Ver detalles de carga"
+                @click="asistir(evento.id_EVENT)"
+              >
+                Asistir
+              </button>
+            </div>
+            <div v-if="isLoadingEvent" class="collapse" v-bind:id="['details' + evento.id_EVENT]">
+              <!--<p> Cargando... </p>-->
+              <div class="loading-icon">
+                <Spinner :color="color" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import NavBar from '../components/header/NavBar'
+import NavBar from "../components/header/NavBar";
 import axios from "axios";
-import {toaster} from './toaster/toaster'
+import { toaster } from "./toaster/toaster";
+import Spinner from "./spinner/Spinner";
+
 export default {
-    name: 'Searcher',
-    components: {
-    NavBar
-  }, 
-  data: function(){
-      return{
-          username: "", 
-          eventos:[] ,
-          search:'', 
-          puedoAsistir:false,
-          toaster
-      }  
-    },
-    created: function() {
+  name: "Searcher",
+  components: {
+    NavBar,
+    Spinner,
+  },
+  data: function () {
+    return {
+      username: "",
+      eventos: [],
+      search: "",
+      puedoAsistir: false,
+      toaster,
+      isLoading: true,
+      isLoadingEvent: false,
+      color: "#94B43B",
+    };
+  },
+  created: function () {
     this.username = this.$route.params.username;
     let self = this;
     axios
-    .get("https://oriun-api.herokuapp.com/eventsall")
-        .then((result) => {
-            self.eventos=result.data; 
-        }).catch((error) => {
-            alert("ERROR Servidor EVENTOS");
-        });
-    }, 
-    methods:{
-      asistir: function(id_EVENT){
-        let self = this;
-        axios
-        .post("https://oriun-api.herokuapp.com/asistirevent?id_user="+self.username+"&id_event="+id_EVENT,{
+      .get("https://oriun-api.herokuapp.com/events?init=-1&size=1000")
+      .then((result) => {
+        self.eventos = result.data;
+        self.isLoading = false;
+      })
+      .catch((error) => {
+        alert("ERROR Servidor EVENTOS");
+      });
+  },
+  methods: {
+    asistir: function (id_EVENT) {
+      let self = this;
+      self.isLoadingEvent = true;
+      axios
+        .post(
+          "https://oriun-api.herokuapp.com/asistirevent?id_user=" +
+            self.username +
+            "&id_event=" +
+            id_EVENT,
+          {
             params: {
-                id_user: self.username, 
-                id_event: id_EVENT
-            }})
-            .then((result) => {
-                self.puedoAsistir = result.data;
-                if(self.puedoAsistir==true){
-                    self.toaster.success("El usuario ha sido regitrado el evento con exito")
-                }
-                else{
-                    self.toaster.failure("El Usuario ya estaba registrado en el evento")
-                }
-            }).catch((error) => {
-                alert("ERROR Servidor ASISTIR EVENTOS");
-            });
-      }
+              id_user: self.username,
+              id_event: id_EVENT,
+            },
+          }
+        )
+        .then((result) => {
+          self.puedoAsistir = result.data;
+          self.isLoadingEvent = false;
+          if (self.puedoAsistir == true) {
+            self.toaster.success(
+              "El usuario ha sido regitrado el evento con exito"
+            );
+          } else {
+            self.toaster.failure(
+              "El Usuario ya estaba registrado en el evento"
+            );
+          }
+        })
+        .catch((error) => {
+          self.isLoadingEvent = false;
+          if (error.response.status == "507") {
+            self.toaster.failure(
+              "El evento ya ha superado el limite de asistentes"
+            );
+          } else {
+            alert("ERROR Servidor ASISTIR EVENTOS");
+          }
+        });
     },
-    computed:{
-      filterEvents: function(){
-          return this.eventos.filter((evento) => {
-              if(evento.event_TITLE.toLowerCase().match(this.search.toLowerCase())
-               || evento.name_SPORT.toLowerCase().match(this.search.toLowerCase()) 
-               || evento.user_NAME.toLowerCase().match(this.search.toLowerCase()) 
-               || evento.name_LOC_SPORT.toLowerCase().match(this.search.toLowerCase())){
-                  return evento
-              }
-          });
+  },
+  computed: {
+    filterEvents: function () {
+      return this.eventos.filter((evento) => {
+        if (
+          evento.event_TITLE.toLowerCase().match(this.search.toLowerCase()) ||
+          evento.name_SPORT.toLowerCase().match(this.search.toLowerCase()) ||
+          evento.user_NAME.toLowerCase().match(this.search.toLowerCase()) ||
+          evento.name_LOC_SPORT.toLowerCase().match(this.search.toLowerCase())
+        ) {
+          return evento;
         }
-    }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
 .plantilla {
-  background: #76232F;
-  min-height: 100vh; }
-  .plantilla .contenedor_p {
-    padding-top: 20px;
-    } 
-    .evento{
-        padding: 2rem;
-        margin: 1rem 0;
+  background: #76232f;
+  min-height: 100vh;
+}
+.plantilla .contenedor_p {
+  padding-top: 20px;
+}
+.evento {
+  padding: 2rem;
+  margin: 1rem 0;
 
-        background-color: #565A5C;
+  background-color: #565a5c;
 
-        color: white;
-        border-radius: 1rem;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        /*align-items: center*/}
-        .columnas{
-            display: flex;
-            justify-content: space-between;
-            flex-wrap: wrap;
-        }
-        #boton-asistir{
-            margin: 5px auto;
-        }
+  color: white;
+  border-radius: 1rem;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  /*align-items: center*/
+}
+.columnas {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+#boton-asistir {
+  margin: 5px auto;
+}
 </style>
